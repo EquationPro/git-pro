216f1c5fb583a1a842fa8975f641d684c3c141da
user007 <22589675962@qq.com>
2019-10-09 01:51:12 +0800

    update second

--- src/main/java/cn/jc/entity/Demo.java
+++ src/main/java/cn/jc/entity/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("this is first");
         System.out.println("this is second");
+        System.out.println("this is idea");
     }
 }
